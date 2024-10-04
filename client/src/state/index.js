8d428e71c294a18b2bc4680cfb9b46dac2b58a92
ARJUNHEMBRAM0007 <1941012905.r.arjunhembram@gmail.com>
2024-10-04 14:09:45 +0530

    first commit

---
+++ client/src/state/index.js
@@ -0,0 +1,19 @@
+import { Mode } from "@mui/icons-material";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    mode: "light"
+};
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === 'light' ? "dark" : 'light';
+        }
+    }
+})
+
+export const { setMode } = globalSlice.actions;
+export default globalSlice.reducer;
